610e1fa2c84f77e938154bc43586ae2415d51560
drbloo <113809641+plo-0318@users.noreply.github.com>
2022-10-18 23:22:48 -0700

    changed config to prod

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
-export const PROD = false;
-export const RESOURCE_URL = PROD
+export const PRODUCTION = true;
+export const RESOURCE_URL = PRODUCTION
   ? 'https://tuffytalkapi.herokuapp.com'
   : 'http://127.0.0.1:5000';
 export const API_URL = 'https://tuffytalkapi.herokuapp.com/api/v1';
 export const PROXY_API_URL = '/api/v1';
-export const USE_PROXY = true;
+export const USE_PROXY = !PRODUCTION;
 
 export const POST_LIMIT = 8;
